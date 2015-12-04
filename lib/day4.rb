6c98f2d869cd9ca016169e2a481c697c3ab1f5c6
Cora Grant <394835+x@users.noreply.github.com>
2015-12-04 17:28:51 -0500

    Complete Day 4 gold challenge

--- lib/day4.rb
+++ lib/day4.rb
@@ -1,3 +1,5 @@
+# --- Day 4: The Ideal Stocking Stuffer ---
+
 require 'digest'
 
 class Day4
@@ -5,13 +7,14 @@ class Day4
     @prefix = prefix
   end
 
-  def mine_advent_coin
+  def mine_coin(difficulty:)
     suffix = 0
+    target_zeros = '0' * difficulty
 
     loop do
       suffix += 1
 
-      if Digest::MD5.hexdigest(@prefix + suffix.to_s).start_with?('00000')
+      if Digest::MD5.hexdigest(@prefix + suffix.to_s).start_with?(target_zeros)
         return suffix
       end
     end
--- spec/day4_spec.rb
+++ spec/day4_spec.rb
@@ -1,8 +1,9 @@
 require 'day4'
 
 RSpec.describe Day4 do
-  it 'finds the lowest numeric suffix that gives an MD5 starting with 00000' do
-    expect(Day4.new('abcdef').mine_advent_coin).to eq 609043
-    expect(Day4.new('pqrstuv').mine_advent_coin).to eq 1048970
+  it 'mines Advent Coins with selectable difficulty' do
+    expect(Day4.new('abcdef').mine_coin(difficulty: 5)).to eq 609043
+    expect(Day4.new('pqrstuv').mine_coin(difficulty: 5)).to eq 1048970
+    expect(Day4.new('ckczppom').mine_coin(difficulty: 6)).to eq 3938038
   end
 end
