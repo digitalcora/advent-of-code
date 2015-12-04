13dc0136b2eeeddf569e427ee89db0219d4bdb5b
Cora Grant <394835+x@users.noreply.github.com>
2015-12-04 15:49:16 -0500

    Complete Day 2 gold challenge

--- lib/day2.rb
+++ lib/day2.rb
@@ -8,7 +8,18 @@ class Day2
   def paper_needed
     @presents.reduce(0) do |total, sides|
       area = sides.combination(2).map{ |pair| pair.reduce(:*) }.reduce(:+) * 2
-      total += area + sides.sort[0..1].reduce(:*)
+      smallest_side_area = sides.sort[0..1].reduce(:*)
+
+      total += area + smallest_side_area
+    end
+  end
+
+  def ribbon_needed
+    @presents.reduce(0) do |total, sides|
+      smallest_perimeter = sides.sort[0..1].reduce(:+) * 2
+      volume = sides.reduce(:*)
+
+      total += smallest_perimeter + volume
     end
   end
 end
--- spec/day2_spec.rb
+++ spec/day2_spec.rb
@@ -6,4 +6,10 @@ RSpec.describe Day2 do
     expect(Day2.new('1x1x10').paper_needed).to eq 43
     expect(Day2.new("2x3x4\n1x1x10").paper_needed).to eq 101
   end
+
+  it 'tells the elves how many feet of ribbon they need' do
+    expect(Day2.new('2x3x4').ribbon_needed).to eq 34
+    expect(Day2.new('1x1x10').ribbon_needed).to eq 14
+    expect(Day2.new("2x3x4\n1x1x10").ribbon_needed).to eq 48
+  end
 end
