2b531da34855ba179261709e6de2b2adac75052e
Cora Grant <394835+x@users.noreply.github.com>
2015-12-31 16:18:08 -0500

    Complete Day 19 gold challenge

--- lib/day19.rb
+++ lib/day19.rb
@@ -32,4 +32,24 @@ class Day19
       end
     end.uniq
   end
+
+  def minimal_steps_from(initial_molecule)
+    steps = [@molecule]
+    replacements = @replacements
+      .map(&:reverse)
+      .sort_by do |(from, to)|
+        from.scan(/[A-Z][a-z]*/).size - to.scan(/[A-Z][a-z]*/).size
+      end
+      .reverse
+
+    loop do
+      from_sequence, to_sequence = replacements.find do |(from_sequence, _)|
+        steps.last.include?(from_sequence)
+      end
+
+      steps << steps.last.sub(/(.*)#{from_sequence}/, "\\1#{to_sequence}")
+
+      return steps[1..-1] if steps.last == initial_molecule
+    end
+  end
 end
--- spec/day19_spec.rb
+++ spec/day19_spec.rb
@@ -9,8 +9,18 @@ RSpec.describe Day19 do
     expect(test2.distinct_molecules.size).to eq 7
   end
 
+  it 'finds the minimal set of replacements that create a molecule from "e"' do
+    test1 = Day19.new(molecule: 'HOH', replacements: test_replacements)
+    test2 = Day19.new(molecule: 'HOHOHO', replacements: test_replacements)
+
+    expect(test1.minimal_steps_from('e').size).to eq 3
+    expect(test2.minimal_steps_from('e').size).to eq 6
+  end
+
   def test_replacements
     [
+      'e => H',
+      'e => O',
       'H => HO',
       'H => OH',
       'O => HH'
