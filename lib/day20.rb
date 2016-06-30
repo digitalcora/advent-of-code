9506a4f6ee52d154181329fa1e0f287f8390f42c
Cora Grant <394835+x@users.noreply.github.com>
2016-06-30 16:25:40 -0400

    Complete Day 20 challenges

---
+++ lib/day20.rb
@@ -0,0 +1,28 @@
+# --- Day 20: Infinite Elves and Infinite Houses ---
+
+class Day20
+  def initialize(target_presents:, house_limit: nil, present_multiplier: 10)
+    @target_presents = target_presents
+    @house_limit = house_limit
+    @present_multiplier = present_multiplier
+  end
+
+  def first_house_number
+    highest_house_number = @target_presents / @present_multiplier
+    house_presents = Array.new(highest_house_number + 1, 0)
+
+    1.upto(highest_house_number).each do |elf_number|
+      elf_last_house = if @house_limit
+        [highest_house_number, elf_number * @house_limit].min
+      else
+        highest_house_number
+      end
+
+      elf_number.step(by: elf_number, to: elf_last_house).each do |house|
+        house_presents[house] += elf_number * @present_multiplier
+      end
+    end
+
+    house_presents.find_index{ |presents| presents >= @target_presents }
+  end
+end
