7d80347b9378453fc1cac0c809010a70cd68f234
Cora Grant <394835+x@users.noreply.github.com>
2018-11-27 17:05:25 -0500

    Complete Day 23 challenges

---
+++ lib/day23.rb
@@ -0,0 +1,60 @@
+# --- Day 23: Opening the Turing Lock ---
+
+class Day23
+  attr_reader :registers
+
+  def initialize(instructions, registers = {a: 0, b: 0})
+    @registers = registers
+    @instructions = instructions.each_line.map do |line|
+      line.split(' ', 2).yield_self do |(inst, rest)|
+        [
+          inst.strip.to_sym,
+          rest.split(',').map(&:strip).map do |arg|
+            case arg
+            when /^[ab]$/ then arg.to_sym
+            when /^(\+|-)\d+$/ then arg.to_i
+            else raise "Unknown argument format: #{arg}"
+            end
+          end
+        ]
+      end
+    end
+  end
+
+  def run!
+    ip = 0
+
+    while ip >= 0 and ip < @instructions.size
+      ip += send(@instructions[ip][0], *@instructions[ip][1])
+    end
+  end
+
+  private
+
+  def hlf(register)
+    @registers[register] /= 2
+    1
+  end
+
+  def inc(register)
+    @registers[register] += 1
+    1
+  end
+
+  def jie(register, offset)
+    @registers[register].even? ? offset : 1
+  end
+
+  def jio(register, offset)
+    @registers[register] == 1 ? offset : 1
+  end
+
+  def jmp(offset)
+    offset
+  end
+
+  def tpl(register)
+    @registers[register] *= 3
+    1
+  end
+end
---
+++ spec/day23_spec.rb
@@ -0,0 +1,83 @@
+require 'day23'
+
+RSpec.describe Day23 do
+  it 'runs the example program' do
+    computer = Day23.new(<<~EOS)
+      inc a
+      jio a, +2
+      tpl a
+      inc a
+    EOS
+
+    computer.run!
+
+    expect(computer.registers[:a]).to eq 2
+  end
+
+  it 'runs a more complex program' do
+    computer = Day23.new(test_program)
+
+    computer.run!
+
+    expect(computer.registers[:b]).to eq 170
+  end
+
+  it 'can preload registers with non-zero values' do
+    computer = Day23.new(test_program, {a: 1, b: 0})
+
+    computer.run!
+
+    expect(computer.registers[:b]).to eq 247
+  end
+
+  def test_program
+    <<~EOS
+      jio a, +16
+      inc a
+      inc a
+      tpl a
+      tpl a
+      tpl a
+      inc a
+      inc a
+      tpl a
+      inc a
+      inc a
+      tpl a
+      tpl a
+      tpl a
+      inc a
+      jmp +23
+      tpl a
+      inc a
+      inc a
+      tpl a
+      inc a
+      inc a
+      tpl a
+      tpl a
+      inc a
+      inc a
+      tpl a
+      inc a
+      tpl a
+      inc a
+      tpl a
+      inc a
+      inc a
+      tpl a
+      inc a
+      tpl a
+      tpl a
+      inc a
+      jio a, +8
+      inc b
+      jie a, +4
+      tpl a
+      inc a
+      jmp +2
+      hlf a
+      jmp -7
+    EOS
+  end
+end
