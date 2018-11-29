1676dc9c24139514439afd6d934919243ef966b0
Cora Grant <394835+x@users.noreply.github.com>
2018-11-29 16:33:48 -0500

    Speed up Day 11 quite a bit

--- lib/day11.rb
+++ lib/day11.rb
@@ -10,23 +10,13 @@ class Day11
   STRAIGHT_RUN = Regexp.union(('a'..'z').each_cons(3).map(&:join))
 
   def next_password
-    next_passwords.find do |password|
-      password !~ BAD_CHARACTERS\
-      && password =~ TWO_PAIRS\
-      && password =~ STRAIGHT_RUN
-    end
-  end
+    password = @current_password
 
-  private
+    password.next! until\
+      !password.match?(BAD_CHARACTERS)\
+      && password.match?(TWO_PAIRS)\
+      && password.match?(STRAIGHT_RUN)
 
-  def next_passwords
-    Enumerator.new do |yielder|
-      password = @current_password
-
-      loop do
-        password.next!
-        yielder << password.dup
-      end
-    end
+    password
   end
 end
