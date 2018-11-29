e4e4a2ca2b814fc7f661a415f66674b91517126c
Cora Grant <394835+x@users.noreply.github.com>
2018-11-29 16:10:30 -0500

    Complete Day 25 challenges!

---
+++ lib/day25.rb
@@ -0,0 +1,27 @@
+# --- Day 25: Let It Snow ---
+
+class Day25
+  def initialize(initial_code)
+    @initial_code = initial_code
+  end
+
+  def code_at(target_row, target_col)
+    row = 1
+    col = 1
+    code = @initial_code
+
+    loop do
+      break code if row == target_row && col == target_col
+
+      if row == 1
+        row = col + 1
+        col = 1
+      else
+        row -= 1
+        col += 1
+      end
+
+      code = code * 252533 % 33554393
+    end
+  end
+end
---
+++ spec/day25_spec.rb
@@ -0,0 +1,7 @@
+require 'day25'
+
+RSpec.describe Day25 do
+  it 'finds the code at a given row and column of the code sheet' do
+    expect(Day25.new(20151125).code_at(4, 3)).to be 21345942
+  end
+end
