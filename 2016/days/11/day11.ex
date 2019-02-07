fd861bfbd04add4bc6d5dafa79df89c70d4095d9
Cora Grant <394835+x@users.noreply.github.com>
2019-02-07 15:27:22 -0500

    Modularize 2016 Day 11 and add a test

--- 2016/days/11/day11.ex
+++ 2016/days/11/day11.ex
@@ -1,174 +1,181 @@
 defmodule Advent.Day11 do
-  def least_steps(input) do
-    input |> parse_input() |> initialize_state() |> shortest_path_length()
-  end
+  defmodule Node do
+    defstruct elevator: 1, pairs: [], top_floor: nil
 
-  defp parse_input(input) do
-    input
-    |> String.trim()
-    |> String.split("\n")
-    |> Enum.reduce(%{}, &parse_floor/2)
-  end
+    def f_score(%__MODULE__{pairs: pairs, top_floor: top_floor}) do
+      pairs |> Enum.flat_map(&Map.values/1) |> Enum.map(&(top_floor - &1)) |> Enum.sum()
+    end
 
-  @floor ~r/^The (?<ordinal>\w+) floor contains (?:nothing relevant|(?<objects>.+))\.$/
-  @object ~r/(?:a (?<element>\w+)(?:-compatible)? (?<type>generator|microchip))+/
-  @ordinals %{"first" => 1, "second" => 2, "third" => 3, "fourth" => 4}
-  @top_floor @ordinals |> Map.values() |> Enum.max()
+    def goal(%__MODULE__{pairs: pairs, top_floor: top_floor}) do
+      %__MODULE__{
+        elevator: top_floor,
+        pairs: List.duplicate(%{generator: top_floor, microchip: top_floor}, Enum.count(pairs)),
+        top_floor: top_floor
+      }
+    end
 
-  defp parse_floor(line, elements) do
-    %{"ordinal" => ordinal, "objects" => objects_string} = Regex.named_captures(@floor, line)
+    def neighbors(%__MODULE__{elevator: elevator, pairs: pairs, top_floor: top_floor}) do
+      destination_floors = Enum.filter([elevator + 1, elevator - 1], &(&1 in 1..top_floor))
 
-    objects_string
-    |> parse_objects()
-    |> Enum.reduce(elements, fn %{element: element, type: type}, elements ->
-      elements
-      |> Map.put_new(element, %{})
-      |> put_in([element, type], Map.fetch!(@ordinals, ordinal))
-    end)
-  end
+      object_combinations =
+        pairs
+        |> Enum.with_index()
+        |> Enum.flat_map(fn {%{generator: gen_floor, microchip: chip_floor}, index} ->
+          [{index, :generator, gen_floor}, {index, :microchip, chip_floor}]
+        end)
+        |> Enum.filter(fn {_, _, floor} -> floor == elevator end)
+        |> combinations()
 
-  defp parse_objects(""), do: []
+      destination_floors
+      |> Enum.flat_map(fn new_floor ->
+        Enum.map(object_combinations, fn combination ->
+          new_pairs =
+            Enum.reduce(combination, pairs, fn {index, type, _}, pairs ->
+              List.update_at(pairs, index, &Map.put(&1, type, new_floor))
+            end)
 
-  defp parse_objects(objects) do
-    objects
-    |> String.split(~r/, and|,|and/)
-    |> Enum.map(&Regex.named_captures(@object, &1))
-    |> Enum.map(fn %{"element" => element, "type" => type} ->
-      %{element: String.to_atom(element), type: String.to_atom(type)}
-    end)
-  end
-
-  defp initialize_state(elements) do
-    %{
-      elevator: 1,
-      pairs: elements |> Map.values() |> Enum.sort()
-    }
-  end
-
-  defp shortest_path_length(start_state) do
-    pathfind(
-      %{
-        open: MapSet.new([start_state]),
-        closed: MapSet.new(),
-        g_score: %{start_state => 0},
-        f_score: %{start_state => ideal_distance_to_goal(start_state)},
-        inverse_f: %{ideal_distance_to_goal(start_state) => MapSet.new([start_state])}
-      },
-      goal_state(start_state)
-    )
-  end
-
-  defp most_promising_state(%{inverse_f: inverse_f}) do
-    Stream.iterate(0, &(&1 + 1))
-    |> Stream.map(&inverse_f[&1])
-    |> Stream.reject(&is_nil/1)
-    |> Stream.reject(&(MapSet.size(&1) == 0))
-    |> Enum.fetch!(0)
-    |> Enum.fetch!(0)
-  end
-
-  defp pathfind(states, goal_state) do
-    current_state = most_promising_state(states)
-
-    if current_state == goal_state do
-      states.g_score[current_state]
-    else
-      current_state
-      |> next_states()
-      |> Enum.reduce(states, fn next_state, states ->
-        g_current = states.g_score[current_state]
-        g_next = states.g_score[next_state]
-        f_next = g_current + ideal_distance_to_goal(next_state)
-
-        cond do
-          next_state in states.open and g_current < g_next ->
-            states
-            |> put_in([:g_score, next_state], g_current + 1)
-            |> update_in([:inverse_f, states.f_score[next_state]], &MapSet.delete(&1, next_state))
-            |> update_in([:inverse_f, f_next], &if(is_nil(&1), do: MapSet.new(), else: &1))
-            |> update_in([:inverse_f, f_next], &MapSet.put(&1, next_state))
-            |> put_in([:f_score, next_state], f_next)
-
-          next_state in states.closed and g_current < g_next ->
-            states
-            |> update_in([:closed], &MapSet.delete(&1, next_state))
-            |> update_in([:open], &MapSet.put(&1, next_state))
-            |> put_in([:g_score, next_state], g_current + 1)
-            |> put_in([:f_score, next_state], f_next)
-            |> update_in([:inverse_f, f_next], &if(is_nil(&1), do: MapSet.new(), else: &1))
-            |> update_in([:inverse_f, f_next], &MapSet.put(&1, next_state))
-
-          next_state not in states.open and next_state not in states.closed ->
-            states
-            |> update_in([:open], &MapSet.put(&1, next_state))
-            |> put_in([:g_score, next_state], g_current + 1)
-            |> put_in([:f_score, next_state], f_next)
-            |> update_in([:inverse_f, f_next], &if(is_nil(&1), do: MapSet.new(), else: &1))
-            |> update_in([:inverse_f, f_next], &MapSet.put(&1, next_state))
-
-          true ->
-            states
-        end
+          %__MODULE__{elevator: new_floor, pairs: Enum.sort(new_pairs), top_floor: top_floor}
+        end)
       end)
-      |> update_in([:open], &MapSet.delete(&1, current_state))
-      |> update_in([:closed], &MapSet.put(&1, current_state))
-      |> update_in([:inverse_f, states.f_score[current_state]], &MapSet.delete(&1, current_state))
-      |> pathfind(goal_state)
+      |> Enum.uniq()
+      |> Enum.filter(&valid?/1)
+    end
+
+    def valid?(%__MODULE__{pairs: pairs}) do
+      floors_with_generators = pairs |> Enum.map(& &1.generator) |> Enum.uniq()
+
+      Enum.all?(floors_with_generators, fn floor ->
+        pairs |> Enum.filter(&(&1.microchip == floor)) |> Enum.all?(&(&1.generator == floor))
+      end)
+    end
+
+    defp combinations([]), do: []
+    defp combinations([item]), do: [[item]]
+
+    defp combinations(items) do
+      [[hd(items)] | Enum.map(tl(items), &[hd(items), &1]) ++ combinations(tl(items))]
     end
   end
 
-  defp goal_state(%{pairs: pairs}) do
-    %{
-      elevator: @top_floor,
-      pairs: List.duplicate(%{generator: @top_floor, microchip: @top_floor}, Enum.count(pairs))
-    }
-  end
+  defmodule Input do
+    def parse(input) do
+      input
+      |> String.trim()
+      |> String.split("\n")
+      |> Enum.reduce(%{}, &parse_floor/2)
+      |> initialize_node()
+    end
 
-  defp next_states(%{elevator: elevator, pairs: pairs}) do
-    object_combinations =
-      pairs
-      |> Enum.with_index()
-      |> Enum.flat_map(fn {%{generator: gen_floor, microchip: chip_floor}, index} ->
-        [{index, :generator, gen_floor}, {index, :microchip, chip_floor}]
+    @floor ~r/^The (?<ordinal>\w+) floor contains (?:nothing relevant|(?<objects>.+))\.$/
+    @object ~r/(?:a (?<element>\w+)(?:-compatible)? (?<type>generator|microchip))+/
+    @ordinals %{"first" => 1, "second" => 2, "third" => 3, "fourth" => 4}
+
+    defp parse_floor(line, elements) do
+      %{"ordinal" => ordinal, "objects" => objects_string} = Regex.named_captures(@floor, line)
+
+      objects_string
+      |> parse_objects()
+      |> Enum.reduce(elements, fn %{element: element, type: type}, elements ->
+        elements
+        |> Map.put_new(element, %{})
+        |> put_in([element, type], Map.fetch!(@ordinals, ordinal))
       end)
-      |> Enum.filter(fn {_, _, floor} -> floor == elevator end)
-      |> combinations()
+    end
 
-    [elevator + 1, elevator - 1]
-    |> Enum.filter(&(&1 in 1..@top_floor))
-    |> Enum.flat_map(fn new_elevator ->
-      Enum.map(object_combinations, fn combination ->
-        new_pairs =
-          Enum.reduce(combination, pairs, fn {index, type, _}, pairs ->
-            List.update_at(pairs, index, &Map.put(&1, type, new_elevator))
-          end)
+    defp parse_objects(""), do: []
 
-        %{elevator: new_elevator, pairs: Enum.sort(new_pairs)}
+    defp parse_objects(objects) do
+      objects
+      |> String.split(~r/, and|,|and/)
+      |> Enum.map(&Regex.named_captures(@object, &1))
+      |> Enum.map(fn %{"element" => element, "type" => type} ->
+        %{element: String.to_atom(element), type: String.to_atom(type)}
       end)
-    end)
-    |> Enum.uniq()
-    |> Enum.filter(&valid_state?/1)
+    end
+
+    defp initialize_node(elements) do
+      %Node{
+        pairs: elements |> Map.values() |> Enum.sort(),
+        top_floor: @ordinals |> Map.values() |> Enum.max()
+      }
+    end
   end
 
-  defp combinations([]), do: []
-  defp combinations([item]), do: [[item]]
+  defmodule AStar do
+    def fewest_steps(%Node{} = start_node) do
+      pathfind(%{
+        goal: Node.goal(start_node),
+        open: MapSet.new([start_node]),
+        closed: MapSet.new(),
+        g_scores: %{start_node => 0},
+        f_scores: %{start_node => Node.f_score(start_node)},
+        inverse_f: %{Node.f_score(start_node) => MapSet.new([start_node])}
+      })
+    end
 
-  defp combinations(items) do
-    [[hd(items)] | Enum.map(tl(items), &[hd(items), &1]) ++ combinations(tl(items))]
+    defp pathfind(state) do
+      current_node = pick_node(state)
+
+      if current_node == state.goal do
+        state.g_scores[current_node]
+      else
+        current_node
+        |> Node.neighbors()
+        |> Enum.reduce(state, fn next_node, state ->
+          g_current = state.g_scores[current_node]
+          g_next = state.g_scores[next_node]
+          f_next = g_current + Node.f_score(next_node)
+
+          if is_nil(g_next) or g_current < g_next do
+            state |> ensure_open(next_node) |> update_scores(next_node, g_current + 1, f_next)
+          else
+            state
+          end
+        end)
+        |> close(current_node)
+        |> pathfind()
+      end
+    end
+
+    defp pick_node(%{inverse_f: inverse_f}) do
+      Stream.iterate(0, &(&1 + 1))
+      |> Stream.map(&inverse_f[&1])
+      |> Stream.reject(&is_nil/1)
+      |> Stream.reject(&(MapSet.size(&1) == 0))
+      |> Enum.fetch!(0)
+      |> Enum.fetch!(0)
+    end
+
+    defp close(state, node) do
+      f_score = state.f_scores[node]
+
+      state
+      |> update_in([:open], &MapSet.delete(&1, node))
+      |> update_in([:closed], &MapSet.put(&1, node))
+      |> update_in([:inverse_f, f_score], &if(is_nil(&1), do: MapSet.new(), else: &1))
+      |> update_in([:inverse_f, f_score], &MapSet.delete(&1, node))
+    end
+
+    defp ensure_open(state, node) do
+      state
+      |> update_in([:closed], &MapSet.delete(&1, node))
+      |> update_in([:open], &MapSet.put(&1, node))
+    end
+
+    defp update_scores(state, node, g_score, f_score) do
+      old_f_score = state.f_scores[node]
+
+      state
+      |> put_in([:g_scores, node], g_score)
+      |> put_in([:f_scores, node], f_score)
+      |> update_in([:inverse_f, old_f_score], &if(is_nil(&1), do: MapSet.new(), else: &1))
+      |> update_in([:inverse_f, old_f_score], &MapSet.delete(&1, node))
+      |> update_in([:inverse_f, f_score], &if(is_nil(&1), do: MapSet.new(), else: &1))
+      |> update_in([:inverse_f, f_score], &MapSet.put(&1, node))
+    end
   end
 
-  defp valid_state?(%{pairs: pairs}) do
-    floors_with_generators = pairs |> Enum.map(& &1.generator) |> Enum.uniq()
-
-    Enum.all?(floors_with_generators, fn floor ->
-      pairs |> Enum.filter(&(&1.microchip == floor)) |> Enum.all?(&(&1.generator == floor))
-    end)
-  end
-
-  defp ideal_distance_to_goal(%{pairs: pairs}) do
-    pairs
-    |> Enum.flat_map(&Map.values/1)
-    |> Enum.map(&(@top_floor - &1))
-    |> Enum.sum()
+  def fewest_steps(input) do
+    input |> Input.parse() |> AStar.fewest_steps()
   end
 end
--- 2016/days/11/day11_test.exs
+++ 2016/days/11/day11_test.exs
@@ -2,15 +2,28 @@ defmodule Advent.Day11Test do
   use ExUnit.Case
   alias Advent.Day11
 
-  test "finds the minimum number of elevator steps to bring all objects to the top floor" do
-    assert Day11.least_steps(test_input()) == 11
+  test "finds the fewest elevator steps required to bring all objects to the top floor" do
+    assert Day11.fewest_steps(test_input_simple()) == 11
   end
 
-  defp test_input do
+  test "finds the fewest steps for a more complex input in a reasonable amount of time" do
+    assert Day11.fewest_steps(test_input_complex()) == 37
+  end
+
+  defp test_input_simple do
     """
     The first floor contains a hydrogen-compatible microchip and a lithium-compatible microchip.
     The second floor contains a hydrogen generator.
     The third floor contains a lithium generator.
+    The fourth floor contains nothing relevant.
+    """
+  end
+
+  defp test_input_complex do
+    """
+    The first floor contains a strontium generator, a strontium-compatible microchip, a plutonium generator, and a plutonium-compatible microchip.
+    The second floor contains a thulium generator, a ruthenium generator, a ruthenium-compatible microchip, a curium generator, and a curium-compatible microchip.
+    The third floor contains a thulium-compatible microchip.
     The fourth floor contains nothing relevant.
     """
   end
