fc70df1d077bc9d2188daeda1586f9809c7cd5e9
Cora Grant <394835+x@users.noreply.github.com>
2019-10-27 12:26:05 -0400

    Fix and improve A* pathfinder

--- 2016/days/11/day11.ex
+++ 2016/days/11/day11.ex
@@ -10,20 +10,12 @@ defmodule Advent.Day11 do
     # The lab always has four floors.
     @top_floor 4
 
-    # Calculate a node's F score (estimated distance to goal) for A* pathfinding, which we say is
+    # Calculate a node's H score (estimated distance to goal) for A* pathfinding, which we say is
     # the sum of the distances of each object from the top floor. Unsure whether this is actually
     # admissible; lower scores are always closer to the goal, but the score is usually greater
     # than the true number of steps required. It gets the shortest path for the input data I had!
-    def f_score(%__MODULE__{pairs: pairs}) do
+    def h_score(%__MODULE__{pairs: pairs}) do
       pairs |> Enum.flat_map(&Map.values/1) |> Enum.map(&(@top_floor - &1)) |> Enum.sum()
-    end
-
-    # Determine the "goal" state for a given starting state: get everything to the top floor.
-    def goal(%__MODULE__{pairs: pairs}) do
-      %__MODULE__{
-        elevator: @top_floor,
-        pairs: List.duplicate(%{generator: @top_floor, microchip: @top_floor}, Enum.count(pairs))
-      }
     end
 
     # Find all valid states that can be reached from a given state in one step.
@@ -129,93 +121,120 @@ defmodule Advent.Day11 do
   end
 
   defmodule AStar do
-    # Generic implementation of A* pathfinding (domain-specific logic is delegated to `Node`).
-    #
-    # Pathfinder state reference:
-    #   `goal` is the node we're trying to reach.
-    #   `open` is the set of nodes we have discovered and still need to explore.
-    #   `closed` is the set of nodes we have discovered and explored (discovered all neighbors).
-    #   `g_scores` maps nodes to G scores (the fewest steps we've ever taken to reach that node).
-    #   `f_scores` maps nodes to F scores (the estimated "distance" from a node to the `goal`).
-    #   `inverse_f` maps F scores to the set of *open* nodes with that score, as an optimization.
+    @moduledoc "Generic A* pathfinder."
 
-    def fewest_steps(%Node{} = start_node) do
+    # Given a struct representing a starting node, returns the fewest number of steps required to
+    # reach a goal, or `nil` if the goal is unreachable. The struct's module is expected to define
+    # these functions:
+    #
+    #   `h_score/1` — Estimate the number of steps to reach the goal from a given node. Must
+    #                 return 0 if-and-only-if the node is at the goal.
+    #   `neighbors/1` — Return a list of nodes that are one step away from a given node.
+    def fewest_steps(%{__struct__: module} = start_node) do
       pathfind(%{
-        goal: Node.goal(start_node),
+        module: module,
         open: MapSet.new([start_node]),
         closed: MapSet.new(),
         g_scores: %{start_node => 0},
-        f_scores: %{start_node => Node.f_score(start_node)},
-        inverse_f: %{Node.f_score(start_node) => MapSet.new([start_node])}
+        h_scores: %{start_node => module.h_score(start_node)},
+        inverse_f: %{module.h_score(start_node) => MapSet.new([start_node])}
       })
     end
 
-    # Pathfinder iteration. Eventually returns the shortest distance to the goal.
+    # Pathfinder iteration. Reference for the `state` map:
+    #   `module` — the module that provides H-score and neighbor functions.
+    #   `open` — the set of nodes we have discovered and still need to explore.
+    #   `closed` — the set of nodes we have discovered and explored (discovered all neighbors).
+    #   `g_scores` — maps nodes to G scores (fewest steps ever taken to reach the node).
+    #   `h_scores` — maps nodes to H scores (estimated distance from the node to the goal).
+    #   `inverse_f` — maps F scores (G+H) to the set of *open* nodes with that score. This acts
+    #                 as a priority queue for the most promising nodes to explore next.
     defp pathfind(state) do
       current = most_promising_node(state)
 
-      if current == state.goal do
-        # We're at the goal, so return the number of steps taken to get here.
-        state.g_scores[current]
-      else
-        # Not at goal. Discover this node's neighbors, then close it and move on.
-        current
-        |> Node.neighbors()
-        |> Enum.reduce(state, fn neighbor, state ->
-          g_current = state.g_scores[current]
-          g_neighbor = state.g_scores[neighbor]
-          f_neighbor = g_current + Node.f_score(neighbor)
+      cond do
+        is_nil(current) ->
+          # We explored all possible nodes and still didn't reach the goal.
+          nil
 
-          # If we've never seen this neighbor, or we've found a shorter path to it than the last
-          # time we saw it, mark it as open and update its scores.
-          if is_nil(g_neighbor) or g_current < g_neighbor do
-            state |> open(neighbor) |> update_scores(neighbor, g_current + 1, f_neighbor)
-          else
-            state
-          end
-        end)
-        |> close(current)
-        |> pathfind()
+        state.h_scores[current] == 0 ->
+          # We're at the goal, so return the number of steps taken to get here.
+          state.g_scores[current]
+
+        true ->
+          # We're not at the goal yet and still have open nodes to explore.
+          # Discover this node's neighbors, then close it and proceed to the next iteration.
+          current
+          |> state.module.neighbors()
+          |> Enum.reduce(state, fn neighbor, state ->
+            g_current = state.g_scores[current]
+            g_neighbor = state.g_scores[neighbor]
+            h_neighbor = state.module.h_score(neighbor)
+
+            # (Re)open this neighbor for consideration if we've either never seen it, or found a
+            # shorter path to it than the last time we saw it.
+            if is_nil(g_neighbor) or g_current + 1 < g_neighbor do
+              state |> open(neighbor, g_current + 1, h_neighbor)
+            else
+              state
+            end
+          end)
+          |> close(current)
+          |> pathfind()
       end
     end
 
-    # Get an arbitrary node from the set of open nodes having the lowest F score. This is kind of
-    # a hack to avoid implementing a true priority queue, but it performs well enough.
+    # Get an arbitrary node from the set of open nodes having the lowest F score.
     defp most_promising_node(%{inverse_f: inverse_f}) do
-      Stream.iterate(0, &(&1 + 1))
+      inverse_f
+      |> Map.keys()
+      |> Enum.sort()
       |> Stream.map(&inverse_f[&1])
       |> Stream.reject(&is_nil/1)
       |> Stream.reject(&(MapSet.size(&1) == 0))
-      |> Enum.fetch!(0)
-      |> Enum.fetch!(0)
+      |> Enum.at(0, MapSet.new())
+      |> Enum.at(0, nil)
     end
 
+    # Close a node, indicating its neighbors have been discovered and scored.
     defp close(state, node) do
-      f_score = state.f_scores[node]
-
       state
       |> update_in([:open], &MapSet.delete(&1, node))
       |> update_in([:closed], &MapSet.put(&1, node))
-      |> update_in([:inverse_f, f_score], &if(is_nil(&1), do: MapSet.new(), else: &1))
-      |> update_in([:inverse_f, f_score], &MapSet.delete(&1, node))
+      |> remove_from_queue(node)
     end
 
-    defp open(state, node) do
+    # Open a node, indicating it should be explored, and assign its G and H scores.
+    defp open(state, node, g_score, h_score) do
       state
+      |> remove_from_queue(node)
       |> update_in([:closed], &MapSet.delete(&1, node))
       |> update_in([:open], &MapSet.put(&1, node))
+      |> put_in([:g_scores, node], g_score)
+      |> put_in([:h_scores, node], h_score)
+      |> add_to_queue(node)
     end
 
-    defp update_scores(state, node, g_score, f_score) do
-      old_f_score = state.f_scores[node]
+    defp add_to_queue(state, node) do
+      f_score = state.g_scores[node] + state.h_scores[node]
 
       state
-      |> put_in([:g_scores, node], g_score)
-      |> put_in([:f_scores, node], f_score)
-      |> update_in([:inverse_f, old_f_score], &if(is_nil(&1), do: MapSet.new(), else: &1))
-      |> update_in([:inverse_f, old_f_score], &MapSet.delete(&1, node))
       |> update_in([:inverse_f, f_score], &if(is_nil(&1), do: MapSet.new(), else: &1))
       |> update_in([:inverse_f, f_score], &MapSet.put(&1, node))
+    end
+
+    defp remove_from_queue(state, node) do
+      g_score = state.g_scores[node]
+      h_score = state.h_scores[node]
+
+      if is_nil(g_score) or is_nil(h_score) do
+        # We can assume the node was not in the queue to begin with.
+        state
+      else
+        state
+        |> update_in([:inverse_f, g_score + h_score], &if(is_nil(&1), do: MapSet.new(), else: &1))
+        |> update_in([:inverse_f, g_score + h_score], &MapSet.delete(&1, node))
+      end
     end
   end
 
