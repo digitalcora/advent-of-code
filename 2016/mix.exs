8a177207a81b04947abbc02e8c7a9e42a9dda049
Cora Grant <394835+x@users.noreply.github.com>
2022-07-11 17:24:37 -0400

    Update Elixir version constraint

--- 2016/mix.exs
+++ 2016/mix.exs
@@ -5,7 +5,7 @@ defmodule Advent.MixProject do
     [
       app: :advent,
       version: "0.1.0",
-      elixir: "~> 1.10",
+      elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: [],
       elixirc_paths: ["days", "lib"],
